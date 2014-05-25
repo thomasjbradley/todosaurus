52bf05e68507a45c382a96beca601cc7ad3d5d65
Thomas J Bradley <hey@thomasjbradley.ca>
2014-05-25 14:03:37 -0400

    Added visible focus to todo items for easier debugging.

--- app.js
+++ app.js
@@ -8,8 +8,17 @@
     buffer = new Todos(),
     actions = new Actions(am, fm, todos, buffer),
     out = document.getElementById('out'),
-    focus = document.getElementById('focus')
+    focus = document.getElementById('focus'),
+    li = document.getElementsByTagName('li')
   ;
+
+  var focusify = function (index) {
+    _.each(li, function (elem, index) {
+      elem.className = '';
+    });
+
+    li[index].className = 'focus'
+  };
 
   _.each(keys, function (elem, index, list) {
     Mousetrap.bind(elem, function () {
@@ -19,6 +28,7 @@
 
   todos.subscribe(function (items) {
     out.innerHTML = '<li>' + items.join('</li><li>') + '</li>';
+    focusify(fm.get());
   });
 
   todos.subscribe(function (items) {
@@ -27,6 +37,7 @@
 
   fm.subscribe(function (index) {
     focus.value = index;
+    focusify(index);
   });
 
   todos.populate(['Watch TV', 'Cook', 'Walk', 'Read', 'Code']);
