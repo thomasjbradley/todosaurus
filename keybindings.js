36e79c4ffe072736e0efa5d4134817ebc5d35e30
Thomas J Bradley <hey@thomasjbradley.ca>
2014-05-26 14:54:20 -0400

    Cleaned up the keybindings a little.

--- keybindings.js
+++ keybindings.js
@@ -5,18 +5,23 @@ var keys = {
   'item:focus:prev:jump' : ['ctrl+u', 'pageup'],
   'item:focus:first'     : ['g g', 'mod+up'],
   'item:focus:last'      : ['G', 'mod+down'],
+
   'item:toggle'          : ['x', 'space'],
   'item:remove'          : ['d d', 'mod+backspace', 'del'],
+
   'item:copy'            : ['Y', 'mod+c'],
   'item:paste:below'     : ['p', 'mod+v'],
   'item:paste:above'     : ['P', 'mod+V'],
   'item:duplicate'       : ['mod+d'],
+
   'item:move:up'         : ['mod+shift+up'],
   'item:move:down'       : ['mod+shift+down'],
+
   'item:new:at-top'      : ['n', 'mod+n'],
   'item:new:at-bottom'   : ['N', 'mod+N'],
   'item:new:after'       : ['o', 'mod+enter'],
   'item:new:before'      : ['O', 'mod+shift+enter'],
+
   'app:search:focus'     : ['/', 'mod+f'],
   'app:search:clear'     : ['command+l', 'ctrl+l']
 };
