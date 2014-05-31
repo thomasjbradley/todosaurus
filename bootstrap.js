7f99712eaa27fc925a7b44bed432279b941ed86f
Thomas J Bradley <hey@thomasjbradley.ca>
2014-05-31 17:22:14 -0400

    Added YepNope script loader to ensure the scipts executed in order.

--- bootstrap.js
+++ bootstrap.js
@@ -2,19 +2,7 @@ var
   gui,
   script,
   env
-  _ = window._ || require('lodash'),
-  menuFiles = [
-    'menu-manager',
-    'file',
-    'edit',
-    'edit-assign-project',
-    'edit-assign-context',
-    'edit-assign-priority',
-    'view',
-    'view-show-project',
-    'view-show-context',
-    'view-show-priority'
-  ]
+  _ = window._ || require('lodash')
 ;
 
 window.isNode = !!(typeof require !== 'undefined');
@@ -33,15 +21,23 @@ if (window.isNode) {
   if (env === 'dev') {
     gui.Window.get().show();
     gui.Window.get().showDevTools();
-    script = document.createElement('script');
-    script.src = 'http://localhost:35729/livereload.js';
-    document.body.appendChild(script);
   }
 
-  _.each(menuFiles, function (item) {
-    script = document.createElement('script');
-    script.src = 'menus/' + item + '.js';
-    document.body.appendChild(script);
+  yepnope({
+    test: (env === 'dev'),
+    yep: 'http://localhost:35729/livereload.js',
+    load: [
+      'menus/menu-manager.js',
+      'menus/file.js',
+      'menus/edit.js',
+      'menus/edit-assign-project.js',
+      'menus/edit-assign-context.js',
+      'menus/edit-assign-priority.js',
+      'menus/view.js',
+      'menus/view-show-project.js',
+      'menus/view-show-context.js',
+      'menus/view-show-priority.js'
+    ]
   });
 
   gui.Window.get().on('focus', function () {
