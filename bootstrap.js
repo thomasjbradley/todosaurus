4b51827daa4590c48d7d6f3511a4e56108cc951c
Thomas J Bradley <hey@thomasjbradley.ca>
2014-06-01 14:09:27 -0400

    Removed YepNope because it was producing empty windows on launch and replaced with document.write().

--- bootstrap.js
+++ bootstrap.js
@@ -16,15 +16,12 @@ if (window.isNode) {
   env = process.env.ENV || 'prod';
   gui = require('nw.gui');
 
-  if (env !== 'dev') {
-    onload = function() {
-      gui.Window.get().show();
-      gui.Window.get().focus();
-    };
-  }
+  onload = function() {
+    gui.Window.get().show();
+    gui.Window.get().focus();
+  };
 
   if (env === 'dev') {
-    gui.Window.get().show();
     gui.Window.get().showDevTools();
   }
 
@@ -41,54 +38,3 @@ if (window.isNode) {
   });
 }
 
-yepnope([
-{
-  test: window.isNode,
-  yep: [
-    'menus/file.js',
-    'menus/edit.js',
-    'menus/edit-toggle-priority.js',
-    'menus/view.js',
-    'menus/view-show-priority.js'
-  ],
-  load: [
-    'utils/fuzzy-match.js',
-    'libs/focus-manager.js',
-    'libs/action-manager.js',
-    'libs/interface-manager.js',
-    'menus/menu-manager.js',
-    'controls/control.js',
-    'controls/list.js',
-    'controls/input.js',
-    'controls/progress.js',
-    'controls/search.js',
-    'controls/jump.js',
-    'controls/tags-search.js',
-    'controls/edit.js',
-    'controls/new.js',
-    'controls/tags.js',
-    'controls/folder-chooser.js',
-    'controls/file-chooser.js',
-    'libs/orderer.js',
-    'libs/grouper.js',
-    'libs/filterer.js',
-    'models/todo.js',
-    'models/todos.js',
-    'models/buffer.js',
-    'storage/storage-manager.js',
-    'storage/localstorage.js',
-    'storage/filesystem.js',
-    'actions.js',
-    'bindings/default.js',
-    'bindings/input.js',
-    'bindings/menu.js',
-    'menus/context-default.js',
-    'menus/context-input.js',
-    'app.js'
-  ]
-},
-{
-  test: (env === 'dev'),
-  yep: 'http://localhost:35729/livereload.js'
-}
-]);
