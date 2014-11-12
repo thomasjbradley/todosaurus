f428374034dda587643c88dea99726bb4d907744
Thomas J Bradley <hey@thomasjbradley.ca>
2014-11-12 17:48:52 -0500

    Converted file shortcut keys to native shortcuts.

--- menus/file.js
+++ menus/file.js
@@ -1,15 +1,22 @@
 menu.file = new gui.MenuItem({ label: 'File' });
 
 menu['file:new'] = new gui.MenuItem({
-  label: 'New Item                                                                                       ⌘N'
+  // label: 'New Item                                                                                       ⌘N'
+  label: 'New Item',
+  key: 'n',
+  modifiers: 'cmd'
 });
 
 menu['file:new-bottom'] = new gui.MenuItem({
-  label: 'New Item at Bottom                                       ⇧⌘N'
+  label: 'New Item at Bottom',
+  key: 'n',
+  modifiers: 'cmd-shift'
 });
 
 menu['file:new-below'] = new gui.MenuItem({
-  label: 'New Item Below                                                              ⌘↵'
+  label: 'New Item Below',
+  key: '\u21B5',
+  modifiers: 'cmd'
 });
 
 menu['file:new-above'] = new gui.MenuItem({
@@ -35,46 +42,62 @@ menu['file:focus-last'] = new gui.MenuItem({
 });
 
 menu['file:focus-jump-up'] = new gui.MenuItem({
-  label: 'Jump Focus Up                                                              ⌃U'
+  label: 'Jump Focus Up',
+  key: 'u',
+  modifiers: 'ctrl'
 });
 
 menu['file:focus-jump-down'] = new gui.MenuItem({
-  label: 'Jump Focus Down                                                   ⌃D'
+  label: 'Jump Focus Down',
+  key: 'd',
+  modifiers: 'ctrl'
 });
 
 menu['file:sep-2'] = new gui.MenuItem({ type: 'separator' });
 
 menu['file:jump'] = new gui.MenuItem({
-  label: 'Jump to Line                                                                           ⌘J'
+  label: 'Jump to Line',
+  key: 'j',
+  modifiers: 'cmd'
 });
 
 menu['file:sep-3'] = new gui.MenuItem({ type: 'separator' });
 
 menu['file:reload'] = new gui.MenuItem({
-  label: 'Reload File                                 ⌘R',
+  label: 'Reload File',
+  key: 'r',
+  modifiers: 'cmd',
   enabled: false
 });
 
 menu['file:sort-file'] = new gui.MenuItem({
-  label: 'Sort File Contents                                ⇧⌘S',
+  label: 'Sort File Contents',
+  key: 's',
+  modifiers: 'cmd-shift',
   enabled: false
 });
 
 menu['file:archive'] = new gui.MenuItem({
-  label: 'Archive All Completed                                 ⌘S',
+  label: 'Archive All Completed',
+  key: 's',
+  modifiers: 'cmd',
   enabled: false
 });
 
 menu['file:sep-4'] = new gui.MenuItem({ type: 'separator' });
 
 menu['file:reveal-in-finder'] = new gui.MenuItem({
-  label: 'Reveal in Finder                                                      ⇧⌘R'
+  label: 'Reveal in Finder',
+  key: 'r',
+  modifiers: 'ctrl-shift'
 });
 
 menu['file:sep-5'] = new gui.MenuItem({ type: 'separator' });
 
 menu['file:switch-directory'] = new gui.MenuItem({
-  label: 'Switch Directory                                                             ⌘O'
+  label: 'Switch Directory',
+  key: 'o',
+  modifiers: 'cmd'
 });
 
 menu.file.submenu = new gui.Menu();
