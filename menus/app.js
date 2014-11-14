2b1cc68343c1c7383e2d96f69a479893260c4c49
Thomas J Bradley <hey@thomasjbradley.ca>
2014-11-14 12:49:54 -0500

    Finished styling the about screen and implemented it in the app.

--- menus/app.js
+++ menus/app.js
@@ -1,5 +1,20 @@
 menu.app = GuiMenu.items[0].submenu;
 
+menu['app:about'] = new gui.MenuItem({
+  label: 'About Todosaurus',
+  click: function () {
+    var about = gui.Window.open('./about.html', {
+      width: 658,
+      height: 358,
+      toolbar: false,
+      focus: true
+    });
+  }
+});
+
+menu.app.insert(menu['app:about'], 0);
+GuiMenu.items[0].submenu.removeAt(1);
+
 menu['app:switch-directory'] = new gui.MenuItem({
   label: 'Switch Directory',
   key: 'o',
